e32c2a2875accb8b978a7835fe9a8a51efffdab8
ottoalves22 <ottoalves22@gmail.com>
2025-01-29 14:48:35 -0300

    labeling some fields

--- dataset_download.py
+++ dataset_download.py
@@ -1,6 +1,9 @@
 #download lichess datasets 
+import sklearn.preprocessing
 import kagglehub
 import pandas as pd
+import sklearn
+
 # Download latest version
 path = kagglehub.dataset_download("datasnaek/chess")
 
@@ -10,4 +13,9 @@ dataset_full = pd.read_csv(f"{path}/games.csv")
 
 dataset = dataset_full[["victory_status", "winner", "moves", "opening_name"]]
 
-print(dataset.head())
+dataset_filtered = dataset.loc[dataset["victory_status"] !=  "outoftime", ["victory_status", "winner", "moves", "opening_name"]]
+
+#cria as labels para os nomes de abertura. COloquei em outro dataframe pra ter como desfazer as labels depois...
+dataset_filtered["opening_name"] = sklearn.preprocessing.LabelEncoder().fit_transform(dataset_filtered["opening_name"])
+
+print(dataset)
